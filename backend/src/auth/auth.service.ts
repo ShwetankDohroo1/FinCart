5ff647bb854ebde08dcc26d1cfbf4c236138e9f3
ShwetankDohroo1 <shwetankwithdohroo@gmail.com>
2025-04-23 23:25:10 +0530

    updated error handling and error messages in auth

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,36 +1,51 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { ForbiddenException, Injectable, UnauthorizedException } from "@nestjs/common";
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcrypt';
 import { PrismaService } from '../prisma.service';
 import { SignupDto } from './dto/signup.dto';
 import { SigninDto } from './dto/signin.dto';
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+
 @Injectable()
 export class AuthService {
     constructor(private prisma: PrismaService, private jwtService: JwtService) { }
 
     //signup
     async signup(dto: SignupDto) {
-        const hashed = await bcrypt.hash(dto.password, 10);
-        const user = await this.prisma.user.create({
-            data: {
-                name: dto.name,
-                email: dto.email,
-                password: hashed,
-                role: dto.role,
-            },
-        });
-        return this.signToken(user.id,user.name, user.email, user.role);
+        try {
+            const hashed = await bcrypt.hash(dto.password, 10);
+            const user = await this.prisma.user.create({
+                data: {
+                    name: dto.name,
+                    email: dto.email,
+                    password: hashed,
+                    role: dto.role,
+                },
+            });
+            return this.signToken(user.id, user.name, user.email, user.role);
+        }
+        catch (err) {
+            if (err instanceof PrismaClientKnownRequestError) {
+                if (err.code === 'P2002') {//when unique not found
+                    throw new ForbiddenException('Credentials already exists Mate.');
+                }
+            }
+            throw err;
+        }
     }
 
     //login
     async signin(dto: SigninDto) {
         const user = await this.prisma.user.findUnique({
-            where: { email: dto.email },
+            where: { email: dto.email },// finding user using email as it is unique.
         });
-        if (!user || !(await bcrypt.compare(dto.password, user.password))) {
-            throw new UnauthorizedException('Invalid Credentials');
+        if (!user) {
+            throw new ForbiddenException('User not Found in DB.');
         }
-        return this.signToken(user.id,user.name, user.email, user.role);
+        if (!(await bcrypt.compare(dto.password, user.password))) {
+            throw new UnauthorizedException('Invalid Password');// passowrd not match or user not found.
+        }
+        return this.signToken(user.id, user.name, user.email, user.role);
     }
 
     //below function is assinging jwt token to user.
--- backend/src/auth/dto/signin.dto.ts
+++ backend/src/auth/dto/signin.dto.ts
@@ -1,6 +1,9 @@
 import { IsEmail, IsString } from 'class-validator';
 
 export class SigninDto {
+  @IsString()
+  name: string;
+
   @IsEmail()
   email: string;
 
